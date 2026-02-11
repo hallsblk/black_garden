ae9b74f2a8e96f3d63bf11b681c74c77a24c6ad1
hallsblk <veronicadxxxi@gmail.com>
2026-02-11 18:50:09 -0300

    movimento da camera com o mouse + personagem acompanha a direção do controle

--- movimento.gd
+++ movimento.gd
@@ -1,9 +1,25 @@
 extends CharacterBody3D
+@onready var skin: MeshInstance3D = %Skin
+@onready var pivot: Node3D = %Pivot
+@onready var camera_3d: Camera3D = %Camera3D
 
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): get_tree().quit()
+	_camera_control(event)
+
+func _camera_control(event):
+	if event is InputEventMouseMotion:
+		pivot.rotation.y -= event.relative.x * 0.003
+		pivot.rotation.x -= event.relative.y * 0.003
+		var limitRadians = deg_to_rad(40)
+		pivot.rotation.x = clampf(pivot.rotation.x,-limitRadians,limitRadians)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,12 +33,20 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (camera_3d.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction.y = 0
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	_set_animation(direction	)
 
 	move_and_slide()
+
+func _set_animation(direction):
+	if direction:
+		var targetAngle = atan2(direction.x, direction.z) - rotation.y
+		skin.rotation.y = lerp_angle(skin.rotation.y, targetAngle, 0.1)
