c50c69fcf70b6661f138d137e6679931edc7ab6e
hallsblk <veronicadxxxi@gmail.com>
2026-02-23 16:38:14 -0300

    inimigo adicionado de novo

---
+++ igual.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+
+var target: Node3D
+
+const SPEED = 4.0
+const JUMP_VELOCITY = 4.5
+
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+
+func _ready():
+	if not target: _set_target()
+	
+func _set_target():
+	target = get_tree().get_first_node_in_group("Jogador")
+
+func _physics_process(delta: float) -> void:
+	var nextPathPosition = navigation_agent_3d.get_next_path_position()
+	
+	var direction = global_position.direction_to(nextPathPosition)
+	var calculateVelocity = direction * SPEED
+	velocity = Vector3(calculateVelocity.x,velocity.y,calculateVelocity.z)
+	
+	look_at(target.global_position)
+	
+
+	move_and_slide()
+	
+	_update_target_position()
+
+func _update_target_position():
+	navigation_agent_3d.target_position = target.global_transform.origin
